c3d0cac7081bce6ad8f28d148cf5c5b7a71c7d45
Daniel Jimenez <barnique2802@gmail.com>
2025-08-15 17:05:51 -0300

    Create structure for react frontend

---
+++ BE/Emocion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Emocion
+    {
+        public int Id { get; private set; }
+        public string Imagen { get; set; }
+        public TipoEmocion Tipo { get; set; }
+        public string Nombre => Tipo.ToString();
+        override public string ToString()
+        {
+            return Nombre;
+        }
+        public Emocion(TipoEmocion tipo, string imagen)
+        {
+            Imagen = imagen;
+            Tipo = tipo;
+        }
+
+
+    }
+}
